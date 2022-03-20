22813dfc90314b49c998cdefe9b1feb8a26c87b4
skidfull <95112430+skidfull@users.noreply.github.com>
2022-03-20 22:21:20 +0800

    Add files via upload

---
+++ pro-c20-ta-boilerplate-main/sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ball,ground
+
+function setup()
+{
+  createCanvas(400,400);
+
+  engine = Engine.create()
+  world = engine.world
+
+  var ball_options = {
+    restitution:0.95,
+    frictionAir:0.01
+  }
+
+  var ground_options = {
+    isStatic:true
+  }
+
+ball = Bodies.circle(100,10,20,ball_options)
+World.add(world,ball)
+
+ground = Bodies.rectangle(10,390,400,20,ground_options)
+World.add(world,ground)
+}
+
+function draw() 
+{
+  background(51);
+ 
+Engine.update(engine)
+ellipse(ball.position.x,ball.position.y,20)
+rect(ground.position.x,ground.position.y,400,20)
+
+
+}
+
